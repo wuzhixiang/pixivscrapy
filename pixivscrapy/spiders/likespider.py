ccf506e912c051677d3c003da99a470dc6111d65
吴志翔 <wuzhixiang1996@outlook.com>
2018-12-06 09:49:38 +0800

    Update likespider.py
    由于p站更新，网页基本由js和ajax加载，故重写了下，但一篇作品下多图只爬了一张，仍待改善

--- pixivscrapy/spiders/likespider.py
+++ pixivscrapy/spiders/likespider.py
@@ -17,7 +17,7 @@ except:
 class Likespider(scrapy.Spider):
 
     name = 'Pixiv_spider'
-    allowed_domains = ['https://www.pixiv.net/']
+    # allowed_domains = ['https://www.pixiv.net/']
     # start_urls = ["https://www.pixiv.net/"]
     username = config.Account_config.get("User_1").get("username")
     passwd = config.Account_config.get("User_1").get("passwd")
@@ -56,59 +56,52 @@ class Likespider(scrapy.Spider):
 
     def parse(self, response):
         like_list = response.xpath("//*[@id='search-result']/div/ul/li/input/@value").extract()
+        like_name = response.xpath("//*[@id='search-result']/div/ul/li/a/@data-user_name").extract()
         if not like_list:
             return
-        for like_id in like_list:
-            for page in range(1, 20):
-                id_url = 'https://www.pixiv.net/member_illust.php?id={0}&type=all&p={1}'.format(like_id, page)
-                yield scrapy.Request(url=id_url, callback=self.parse_item, dont_filter=True, priority=3,
-                                     meta={'cookiejar': response.meta['cookiejar']})
-        
+        for like_i in range(len(like_list)):
+            id_url = 'https://www.pixiv.net/ajax/user/{0}/profile/all'.format(like_list[like_i])
+            path_1 = self.save_path + "\\" + name_manage(like_name[like_i])
+            if not os.path.exists(path_1):
+                os.makedirs(path_1)
+            yield scrapy.Request(url=id_url, callback=self.parse_item, dont_filter=True, priority=3,
+                                 meta={'cookiejar': response.meta['cookiejar'], 'file_path': path_1})
 
     def parse_item(self, response):
-        like_user_name = response.xpath("//*[@id='wrapper']/div[1]/div[2]/div/div[1]/a/h1/text()").extract()[0]
-        path_1 = self.save_path + "\\" + name_manage(like_user_name)
-        if not os.path.exists(path_1):
-            os.makedirs(path_1)
-        re_id = re.compile("id=(.*)")
-        img_src_set = response.xpath("//*[@id='wrapper']/div[1]/div[1]/div/div[4]/ul/li/a[1]/@href").extract()
-        img_name_set = response.xpath("//*[@id='wrapper']/div[1]/div[1]/div/div[4]/ul/li/a[2]/h1/@title").extract()
-        #img_src_set = list(set(img_src))
-        #img_src_set.sort(key=img_src.index)
-        #img_name_set = list(set(img_name))
-        #img_name_set.sort(key=img_name.index)
-        like_picture = []
-        for counter in range(0, len(img_src_set)):
-            if re_id.findall(img_src_set[counter]):
-                like_picture_info = {'src': re_id.findall(img_src_set[counter])[0], 'name': img_name_set[counter]}
-                like_picture.append(like_picture_info)
-        for picture in like_picture:
-            file_name = name_manage(picture['name']) + ''
-            big_picture_url = "https://www.pixiv.net/member_illust.php?mode=medium&illust_id={0}".format(picture['src'])
+        # print response.body
+        re_id = re.compile('"(\d*?)":null')
+        img_id = re_id.findall(response.text)
+        if not img_id:
+            return
+        for picture in img_id:
+            big_picture_url = "https://www.pixiv.net/ajax/illust/{0}".format(picture)
             yield scrapy.Request(url=big_picture_url, callback=self.parse_item_item, dont_filter=True, priority=2,
-                           meta={'cookiejar': response.meta['cookiejar'], 'file_path': path_1, 'file_name': file_name,
-                                 'picture_id': picture['src']})
+                           meta={'cookiejar': response.meta['cookiejar'], 'file_path': response.meta['file_path'], 'picture_id': picture})
 
     def parse_item_item(self, response):
-        big_picture_src = response.xpath("//*[@id='wrapper']/div[2]/div/img/@data-src").extract()
-
+        re_name = re.compile('"illustTitle":"(.*?)"')
+        re_url = re.compile('"regular":"(.*?)"')
+        img_name = name_manage(re_name.findall(response.text)[0].decode('gb2312'))
+        img_url = re_url.findall(response.text)[0].replace('\\','')
         picture_id = response.meta['picture_id']
-        if big_picture_src:
+        if img_name and img_url:
             url_re = re.compile('https://i.pximg.net/(.*)')
-            picture_path = url_re.findall(big_picture_src[0])
+            picture_path = url_re.findall(img_url)
             if not picture_path:
                 return
-            postfix = big_picture_src[0].split('.')[-1]
-            file_path = response.meta['file_path'] + '\\' + response.meta['file_name'] + '_' + picture_id + '.' + postfix
+            postfix = img_url.split('.')[-1]
+            file_path = response.meta['file_path'] + '\\' + img_name + '_' + picture_id + '.' + postfix
             header = {'authority': 'i.pximg.net', 'method': 'GET', 'path': picture_path[0],
                       'scheme': 'https', 'accept': 'image/webp,image/apng,image/*,*/*;q=0.8', 'accept-encoding': 'gzip, deflate, br',
                       'accept-language': 'zh-CN,zh;q=0.8,en;q=0.6','referer': response.url,
                       'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
-            request = urllib2.Request(big_picture_src[0], None, header)
+            request = urllib2.Request(img_url, None, header)
             response = urllib2.urlopen(request)
             with open(file_path, "wb") as f:
                 f.write(response.read())
         else:
+            return
+            '''
             redirect_url = response.url.replace("medium", "manga")
             file_path = response.meta['file_path'] + '\\' + response.meta['file_name']
             if not os.path.exists(file_path):
@@ -116,11 +109,12 @@ class Likespider(scrapy.Spider):
             yield scrapy.Request(url=redirect_url, callback=self.parse_item_item_item, dont_filter=True, priority=1,
                                  meta={'cookiejar': response.meta['cookiejar'], 'file_path': file_path,
                                        'file_name': response.meta['file_name'], 'picture_id': picture_id})
+                                       '''
 
     def parse_item_item_item(self, response):
         big_picture_src = response.xpath("//*[@id='main']/section/div/img/@data-src").extract()
         picture_id = response.meta['picture_id']
-        url_re = re.compile('https://i.pximg.net/(.*)')
+        url_re = re.compile('https://i.pximg.net/img-master/(.*)_master1200.jpg')
         file_path = response.meta['file_path']
         file_name = response.meta['file_name']
         for counter in range(0, len(big_picture_src)):
@@ -128,13 +122,16 @@ class Likespider(scrapy.Spider):
             picture_path = url_re.findall(picture)
             if picture_path:
                 post_fix = picture.split('.')[-1]
+                picture_path_c = "img-original/" + picture_path[0] + '.' + post_fix
+                picture_path[0] = "https://i.pximg.net/img-original/" + picture_path[0] + '.' + post_fix
+
                 file_path_at = file_path + '\\' + file_name + '_' + picture_id + '_p' + str(counter) + '.' + post_fix
-                header = {'authority': 'i.pximg.net', 'method': 'GET', 'path': picture_path[0],
+                header = {'authority': 'i.pximg.net', 'method': 'GET', 'path': picture_path_c,
                           'scheme': 'https', 'accept': 'image/webp,image/apng,image/*,*/*;q=0.8',
                           'accept-encoding': 'gzip, deflate, br',
                           'accept-language': 'zh-CN,zh;q=0.8,en;q=0.6', 'referer': response.url,
                           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
-                request = urllib2.Request(picture, None, header)
+                request = urllib2.Request(picture_path[0], None, header)
                 response = urllib2.urlopen(request)
                 with open(file_path_at, "wb") as f:
                     f.write(response.read())
